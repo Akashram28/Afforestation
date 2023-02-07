b340d3554839acf60752b2af53065499bd72a98d
AdvaitSan <124705956+AdvaitSan@users.noreply.github.com>
2023-02-08 02:23:37 +0530

    added treedensity

--- foresttype.py
+++ foresttype.py
@@ -7,10 +7,10 @@ class ForestType(Resource):
     climate={'Moist tropical forest':'Hot','Dry tropical forest':'Arid','Alpine forest':'Cold','Monatane temperte forest':'warm','Montaine sub tropical forest':'Cool'}
     data = {
     'Moist tropical forest' : {
-        'humidity' : 'veryHigh',
+        'humidity' : 'very high',
         'temp' : 'high',
-        'rainfall' : 'veryHigh',
-        'elevaion' : 'veryLow'
+        'rainfall' : 'very high',
+        'elevaion' : 'very low'
         },
     'Dry tropical forest' : {
         'humidity' : 'low',
@@ -32,15 +32,15 @@ class ForestType(Resource):
         'elevaion' : 'medium'
     },    
     'Alpine forest':{
-        'humidity' : 'veryLow',
-        'temp' : 'veryLow',
-        'rainfall' : 'veryLow',
-        'elevaion' : 'veryHigh'
+        'humidity' : 'very low',
+        'temp' : 'very low',
+        'rainfall' : 'very low',
+        'elevaion' : 'very high'
         
     }
     }
     keys = ['humidity', 'temp', 'rainfall', 'elevation']
-    values = ['veryHigh', 'high', 'medium', 'low', 'veryLow']
+    values = ['very high', 'high', 'medium', 'low', 'very low']
     solutions = {'Alpine Forest':'As alpine forests are mostly chopped to use their long straight wood. Selective logging is a method that can be used to meet the needs of both the forests and humans seeking economical resources. Selective logging is the removal of trees within a stand based on size limitations. This technique allows for forest regeneration to occur between and after the selective harvest cycles.',
                  'Dry Tropical forest':'Site water management also plays an essential role in site preparation in certain areas. If drainage is not considered in some low-lying afforestation lands, the afforestation will fail.Flooding is a treatment that use channel or dikes to guide the water from afforestation site with high moisture, like coastal and riparian lands, shrimp ponds, swamps. Flooding or irrigation also can reduce the salt and alkali content of saline soil . Irrigation is an essential way in improving site water conditions in water-deficient areas, such as the Middle East, South Africa, China, India, especially for the cultivation of timber forests ',
                  'Moist Tropical Forest':'Building blocks for tropical rainforest conservation include ecotourism and rehabilitation. Reforestation and restoration are common practices in certain areas to try to increase tropical rainforest density. By communicating with the local people living in, and around, the rainforest, conservationists can learn more about what might allow them to best focus their efforts.[1] Rainforests are globally important to sustainability and preservation of biodiversity. Although they may vary in location and inhabited species of plants and animals, they remain important worldwide for their abundance of natural resources and for the ecosystem services. It is important to take into consideration the differing species and the biodiversity that exists across different rainforest types in order to accurately implement methods of conservation',
@@ -51,10 +51,10 @@ class ForestType(Resource):
 
     def get(self,humidity,temp,rainfall,elevation):
          user_input = {
-            'humidity' : humidity,
-            'temp' : temp,
-            'rainfall' : rainfall,
-            'elevaion' : elevation}
+            'humidity' : 'low',
+            'temp' : 'high',
+            'rainfall' : 'low',
+            'elevaion' : 'very high'}
         
          grades = []
 
@@ -69,9 +69,15 @@ class ForestType(Resource):
             print('grade ' + str(grade))
             grades.append(grade)
          print(max(grades))
-        forest = list(self.data.keys())[grades.index(max(grades))]
-         return {"ForestType":forest,"Solution":solutions[forest],"Climate" : climate[forest]}
+         return {"ForestType":(list(self.data.keys())[grades.index(max(grades))]),"Solution":""}
+     
+class TreeDensity(Resource):
+    def get(self,forest,area):
+        Plants={'Moist Tropical forest':{'Rosewood':40  ,'Bonsum':300 ,'Multiplier':1.2},'Dry Tropical forest':{'Acacia':40 ,'Sal':25,'Multiplier':0.8},
+                'Montane Temperte forest':{'Oak':60  ,'Maple':50   ,'Multiplier':1},'Montane Sub tropical forest':{"Cinnamon":60  ,'Sandal':40   ,'Multiplier':1},
+                'Alpine forest':{'Honey Suckle':20  ,'Black Juniper':17    ,'Multiplier':0.8}}
         
+api.add_resource(TreeDensity,'/treedensity/<string:forest>/<int:area>')
 api.add_resource(ForestType,'/foresttype/<string:humidity>/<string:temp>/<string:rainfall>/<string:elevation>')
 if __name__ == "__main__":
     app.run(debug=True)
